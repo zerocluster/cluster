6e2e9a1f7082af5d403f06a51d5e5cdb41337ffc
zdm <zdm@softvisio.net>
2022-02-08 02:32:50 +0200

    fix: app mode cli

--- bin/main.js
+++ bin/main.js
@@ -9,8 +9,3 @@ const app = new App();
 const res = await app.run();
 
 if ( !res.ok ) process.exit( 1 );
-
-// console.log( process.env );
-console.log( "NODE_ENV", process.env.NODE_ENV );
-console.log( "NODE_OPTIONS", process.env.NODE_OPTIONS );
-process.exit();
